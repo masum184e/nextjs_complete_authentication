c11c8a89d4584e623e8653d088db95f791687e93
masum184e <masum184e@gmail.com>
2024-06-19 17:20:00 +0600

    user schema added

---
+++ src/models/userSchema.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+    }
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
