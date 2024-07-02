a275f33007a14e23dff1a53679892eb8cc665285
masum184e <masum184e@gmail.com>
2024-07-03 01:20:03 +0600

    middleware updated

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,51 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
+import { jwtVerify } from 'jose';
 
-export const middleware = (request) => {
+const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+
+async function verifyToken(token) {
+    try {
+        const { payload } = await jwtVerify(token, secret);
+        return payload;
+    } catch (e) {
+        return null;
+    }
+}
+
+export const middleware = async (request) => {
     const path = request.nextUrl.pathname;
-    const isPublicPath = ['/login', '/registration'].includes(path);
+    const isPublicPath = ['/login', '/registration', '/admin/login'].includes(path);
 
     const authorizationToken = request.cookies.get(process.env.COOKIE_KEY)?.value || '';
+    const { userId, adminId } = await verifyToken(authorizationToken) || "";
 
-    if (isPublicPath && authorizationToken) {
-        return NextResponse.redirect(new URL('/profile', request.nextUrl).toString());
-    }
-    if (!isPublicPath && !authorizationToken) {
+    if (!isPublicPath && !adminId && !userId) {
         return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
     }
 
-    return undefined;
+    if (isPublicPath && userId && !adminId) {
+        return NextResponse.redirect(new URL('/profile', request.nextUrl).toString());
+    }
+
+    if (isPublicPath && !userId && adminId) {
+        return NextResponse.redirect(new URL('/admin', request.nextUrl).toString());
+    }
+
+    if (path.includes("/admin") && userId && !adminId) {
+        return NextResponse.redirect(new URL('/profile', request.nextUrl).toString());
+    }
+    if (path.includes("/profile") && !userId && adminId) {
+        return NextResponse.redirect(new URL('/admin', request.nextUrl).toString());
+    }
+
+    return NextResponse.next();
 }
+
 export const config = {
     matcher: [
         '/profile',
         '/login',
         '/registration',
+        '/admin/:path*'
     ]
 }
