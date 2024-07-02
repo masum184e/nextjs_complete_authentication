6d0a3bf3d74e275d8847eb58922afe3e114fd941
masum184e <masum184e@gmail.com>
2024-07-02 23:51:42 +0600

    dashboard integrated

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -2,8 +2,13 @@
 import Link from "next/link"
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { FaUsers } from "react-icons/fa6";
+import { MdDashboard } from "react-icons/md";
+import { IoLogOut } from "react-icons/io5";
+import { useRouter } from "next/navigation";
 
 const Admin = () => {
+  const router = useRouter();
   const [authorizedUser, setAuthorizedUser] = useState(null);
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -20,17 +25,41 @@ const Admin = () => {
     };
     fetchProfileData();
   }, []);
+
+  const handleSignOut = async () => {
+    try {
+      const response = await axios.get("/api/logout");
+      if (response.data.success) {
+        console.log("Logout success", response.data);
+        router.push("/login");
+      } else {
+        console.error("Error during sign out:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error during sign out:", error);
+    }
+  };
+
   return (
     <>
       <div className="flex gap-4">
-        <div className="">
+        <div className="border-r rounded bg-gray-100 p-2 w-2/12">
           <ul>
-            <li><Link href="/admin">Dashboard</Link></li>
-            <li><Link href="/users">Users</Link></li>
-            <li><Link href="/users">Log Out</Link></li>
+            <li className="text-xl text-gray-700 hover:text-black pr-2 mb-2 hover:font-medium flex items-center gap-2">
+              <FaUsers />
+              <Link href="/admin">Dashboard</Link>
+              </li>
+            <li className="text-xl text-gray-700 hover:text-black pr-2 mb-2 hover:font-medium flex items-center gap-2">
+            <MdDashboard />
+              <Link href="/users">Users</Link>
+              </li>
+            <li onClick={handleSignOut} className="text-xl text-gray-700 hover:text-black pr-2 mb-2 hover:font-medium flex items-center gap-2">
+            <IoLogOut />
+              <button >Log Out</button>
+              </li>
           </ul>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 w-8/12">
           <div className="w-32 h-32 border rounded text-center p-4 flex flex-col justify-center items-center">
             <span className="text-4xl font-bold">10</span>
             <span>User</span>
