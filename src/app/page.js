1ed4e71b99e74960d6f8f31dbaf9f0f5bc801d99
masum184e <masum184e@gmail.com>
2024-07-02 14:50:20 +0600

    banner updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,57 @@
 import Link from "next/link";
+import Image from 'next/image'
 
-import { FaAnglesRight } from "react-icons/fa6";
 import { GrGraphQl } from "react-icons/gr";
 import { SiExpress } from "react-icons/si";
 import { IoLogoFirebase } from "react-icons/io5";
 import { SiAdguard } from "react-icons/si";
 import { TbBrandOauth } from "react-icons/tb";
+import { FaAnglesRight } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex">
+        <div className="flex-1 pt-20">
+          <h3 className="text-[#29444d] text-xl font-semibold mb-4">
+            Why Next.JS _____
+          </h3>
+          <h1 className="text-6xl font-bold text-[#29444d]">
+            <span
+              style={{
+                color: "transparent",
+                WebkitTextStroke: "1px yellow",
+              }}
+            >
+              Fast, Efficient
+            </span>
+            <br />
+            <span className="">Fullstack</span>
+            <span className="ml-2 text-[#fea511]">Framework</span>
+          </h1>
+          <p className="my-4">
+            Next.js is a React framework that gives you building blocks to create web applications. By framework, we mean Next.js handles the tooling and configuration needed for React, and provides additional structure, features, and optimizations for your application.
+          </p>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://nextjs.org/docs"
+            className="rounded-full font-semibold bg-[#000000] text-white px-4 p-1 inline-flex items-center gap-2"
+          >
+            <span> Get Started</span>
+            <FaAnglesRight className="animate-pulse" />
+          </Link>
+        </div>
+        <div className="flex-1">
+          <Image
+            src="/images/banner.png"
+            alt="BANNER"
+            height={1000}
+            width={1000}
+          />
+        </div>
+      </div>
+      <div className="flex justify-between mt-16 pt-2">
         <Link href="" className="border-[#000] border-2 w-50 h-50 flex flex-col rounded p-5 text-[#ee0097]">
           <GrGraphQl className="mx-auto text-4xl" />
           <h4>GraphQL Authentication</h4>
