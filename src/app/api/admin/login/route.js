92accc0246e156129a2399d0e5684aa010782bc1
masum184e <masum184e@gmail.com>
2024-07-02 22:25:28 +0600

    admin login controller added

---
+++ src/app/api/admin/login/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import UserModel from "@/models/userSchema";
+import databaseConnection from "@/config/databaseConnection";
+
+databaseConnection(process.env.DATABASE_URL, process.env.DATABASE_NAME);
+
+export const POST = async (request) => {
+    try {
+        const { email, password } = await request.json();
+
+        const existingUser = await UserModel.findOne({ email });
+        console.log(existingUser)
+        if (!existingUser || existingUser.role !== "admin") {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid user"
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid password"
+            });
+        }
+
+        const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.TOKEN_EXPIRES });
+
+        const response = NextResponse.json({
+            success: true,
+            message: "Login successful"
+        });
+
+        response.cookies.set(process.env.COOKIE_KEY, token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        });
+
+        return response;
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: "Server error: " + error.message
+        });
+    }
+};
