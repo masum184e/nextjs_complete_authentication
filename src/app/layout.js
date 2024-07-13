85fbd18d9fe0d0c11a2aa0cff0a77985932241aa
masum184e <masum184e@gmail.com>
2024-07-13 10:10:39 +0600

    react-hot-toast installed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import 'flowbite/dist/flowbite.css';
 import NavBar from '@/components/layout/NavBar';
+import { Toaster } from 'react-hot-toast';
 
 export const metadata = {
   title: 'Next.js Complete Authentication',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <NavBar />
+        <Toaster position="top-right" />
         <main className="max-w-screen-xl mx-auto">
           {children}
         </main>
